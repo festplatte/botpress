83a4feaef9b8fe5fd3ea5a152d6a41996770d077
festplatte <mich.gabler@gmx.de>
2020-02-23 19:59:04 +0100

    Replace yarn with npm (#2)
    * Install with npm instead of yarn
    
    * Update packaging
    
    * Update package

--- bin/package.js
+++ bin/package.js
@@ -11,6 +11,8 @@ var _glob = _interopRequireDefault(require("glob"));
 
 var _path = _interopRequireDefault(require("path"));
 
+var _fs = _interopRequireDefault(require("fs"));
+
 var _rimraf = _interopRequireDefault(require("rimraf"));
 
 var _util = require("util");
@@ -19,60 +21,66 @@ var _log = require("./log");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const tar = require('tar');
+const tar = require("tar");
 
 const execAsync = (0, _util.promisify)(_child_process.exec);
+let packageJson;
 
 var _default = async argv => {
   const modulePath = _path.default.resolve(argv.path || process.cwd());
 
   const out = argv.out;
+  packageJson = require(_path.default.join(modulePath, "package.json"));
 
   try {
     await installProductionDeps(modulePath);
     await zipFiles(modulePath, out);
   } catch (err) {
-    return (0, _log.error)(`Error packaging module: ${err.message || ''} ${err.cmd || ''} ${err.stderr || ''}`);
+    return (0, _log.error)(`Error packaging module: ${err.message || ""} ${err.cmd || ""} ${err.stderr || ""}`);
   } finally {
     await cleanup(modulePath);
   }
 
-  (0, _log.normal)('Package completed');
+  (0, _log.normal)("Package completed");
 };
 
 exports.default = _default;
 
 const getTargetOSConfig = () => {
-  if (process.argv.find(x => x.toLowerCase() === '--win32')) {
-    return 'win32';
-  } else if (process.argv.find(x => x.toLowerCase() === '--linux')) {
-    return 'linux';
+  if (process.argv.find(x => x.toLowerCase() === "--win32")) {
+    return "win32";
+  } else if (process.argv.find(x => x.toLowerCase() === "--linux")) {
+    return "linux";
   } else {
-    return 'darwin';
+    return "darwin";
   }
 };
 
 async function installProductionDeps(modulePath) {
-  (0, _log.debug)('Installing production modules...');
-  const {
-    stdout
-  } = await execAsync(`cross-env npm_config_target_platform=${getTargetOSConfig()} yarn install --modules-folder node_production_modules --production --no-lockfile --ignore-scripts --force`, {
-    cwd: modulePath
-  });
-  (0, _log.debug)(stdout);
+  if (packageJson.dependencies) {
+    (0, _log.debug)("Installing production modules...");
+    const {
+      stdout
+    } = await execAsync(`cross-env npm_config_target_platform=${getTargetOSConfig()} && mv node_modules node_modules_temp && npm ci --production && mv node_modules node_production_modules && mv node_modules_temp node_modules`, {
+      cwd: modulePath
+    });
+    (0, _log.debug)(stdout);
+  } else {
+    (0, _log.debug)("No production modules found, creating empty node_production_modules folder...");
+
+    _fs.default.mkdirSync(_path.default.join(modulePath, "node_production_modules"));
+  }
 }
 
 async function cleanup(modulePath) {
-  (0, _log.debug)('Cleaning up temporary files...');
+  (0, _log.debug)("Cleaning up temporary files...");
 
-  _rimraf.default.sync(_path.default.join(modulePath, 'node_production_modules'));
+  _rimraf.default.sync(_path.default.join(modulePath, "node_production_modules"));
 }
 
 async function zipFiles(modulePath, outPath) {
-  const packageJson = require(_path.default.join(modulePath, 'package.json'));
-
-  outPath = outPath.replace(/%name%/gi, packageJson.name.replace(/[^\w-]/gi, '_'));
-  outPath = outPath.replace(/%version%/gi, packageJson.version.replace(/[^\w-]/gi, '_'));
+  outPath = outPath.replace(/%name%/gi, packageJson.name.replace(/[^\w-]/gi, "_"));
+  outPath = outPath.replace(/%version%/gi, packageJson.version.replace(/[^\w-]/gi, "_"));
 
   if (!_path.default.isAbsolute(outPath)) {
     outPath = _path.default.join(modulePath, outPath);
@@ -80,14 +88,14 @@ async function zipFiles(modulePath, outPath) {
 
   (0, _log.debug)(`Writing to "${outPath}"`);
 
-  const files = _glob.default.sync('**/*', {
+  const files = _glob.default.sync("**/*", {
     cwd: modulePath,
     nodir: true,
     dot: true,
-    ignore: ['node_modules/**', 'src/**']
+    ignore: ["node_modules/**", "src/**"]
   });
 
-  (0, _log.debug)(`Zipping ${files.length} file${files.length === 1 ? '' : 's'}...`);
+  (0, _log.debug)(`Zipping ${files.length} file${files.length === 1 ? "" : "s"}...`);
   await tar.create({
     gzip: true,
     follow: true,
--- src/package.ts
+++ src/package.ts
@@ -1,76 +1,98 @@
-import { exec } from 'child_process'
-import glob from 'glob'
-import path from 'path'
-import rimraf from 'rimraf'
-import { promisify } from 'util'
+import { exec } from "child_process";
+import glob from "glob";
+import path from "path";
+import fs from "fs";
+import rimraf from "rimraf";
+import { promisify } from "util";
 
-import { debug, error, normal } from './log'
+import { debug, error, normal } from "./log";
 
-const tar = require('tar')
+const tar = require("tar");
 
-const execAsync = promisify(exec)
+const execAsync = promisify(exec);
+
+let packageJson: any;
 
 export default async (argv: any) => {
-  const modulePath = path.resolve(argv.path || process.cwd())
-  const out = argv.out
+  const modulePath = path.resolve(argv.path || process.cwd());
+  const out = argv.out;
+  packageJson = require(path.join(modulePath, "package.json"));
 
   try {
-    await installProductionDeps(modulePath)
-    await zipFiles(modulePath, out)
+    await installProductionDeps(modulePath);
+    await zipFiles(modulePath, out);
   } catch (err) {
-    return error(`Error packaging module: ${err.message || ''} ${err.cmd || ''} ${err.stderr || ''}`)
+    return error(
+      `Error packaging module: ${err.message || ""} ${err.cmd ||
+        ""} ${err.stderr || ""}`
+    );
   } finally {
-    await cleanup(modulePath)
+    await cleanup(modulePath);
   }
 
-  normal('Package completed')
-}
+  normal("Package completed");
+};
 
 const getTargetOSConfig = () => {
-  if (process.argv.find(x => x.toLowerCase() === '--win32')) {
-    return 'win32'
-  } else if (process.argv.find(x => x.toLowerCase() === '--linux')) {
-    return 'linux'
+  if (process.argv.find(x => x.toLowerCase() === "--win32")) {
+    return "win32";
+  } else if (process.argv.find(x => x.toLowerCase() === "--linux")) {
+    return "linux";
   } else {
-    return 'darwin'
+    return "darwin";
   }
-}
+};
 
 async function installProductionDeps(modulePath) {
-  debug('Installing production modules...')
-  const { stdout } = await execAsync(
-    `cross-env npm_config_target_platform=${getTargetOSConfig()} yarn install --modules-folder node_production_modules --production --no-lockfile --ignore-scripts --force`,
-    {
-      cwd: modulePath
-    }
-  )
-  debug(stdout)
+  if (packageJson.dependencies) {
+    debug("Installing production modules...");
+    const { stdout } = await execAsync(
+      `cross-env npm_config_target_platform=${getTargetOSConfig()} && mv node_modules node_modules_temp && npm ci --production && mv node_modules node_production_modules && mv node_modules_temp node_modules`,
+      {
+        cwd: modulePath
+      }
+    );
+    debug(stdout);
+  } else {
+    debug(
+      "No production modules found, creating empty node_production_modules folder..."
+    );
+    fs.mkdirSync(path.join(modulePath, "node_production_modules"));
+  }
 }
 
 async function cleanup(modulePath) {
-  debug('Cleaning up temporary files...')
-  rimraf.sync(path.join(modulePath, 'node_production_modules'))
+  debug("Cleaning up temporary files...");
+  rimraf.sync(path.join(modulePath, "node_production_modules"));
 }
 
 async function zipFiles(modulePath, outPath) {
-  const packageJson = require(path.join(modulePath, 'package.json'))
-  outPath = outPath.replace(/%name%/gi, packageJson.name.replace(/[^\w-]/gi, '_'))
-  outPath = outPath.replace(/%version%/gi, packageJson.version.replace(/[^\w-]/gi, '_'))
+  outPath = outPath.replace(
+    /%name%/gi,
+    packageJson.name.replace(/[^\w-]/gi, "_")
+  );
+  outPath = outPath.replace(
+    /%version%/gi,
+    packageJson.version.replace(/[^\w-]/gi, "_")
+  );
 
   if (!path.isAbsolute(outPath)) {
-    outPath = path.join(modulePath, outPath)
+    outPath = path.join(modulePath, outPath);
   }
 
-  debug(`Writing to "${outPath}"`)
+  debug(`Writing to "${outPath}"`);
 
-  const files = glob.sync('**/*', {
+  const files = glob.sync("**/*", {
     cwd: modulePath,
     nodir: true,
     dot: true,
-    ignore: ['node_modules/**', 'src/**']
-  })
+    ignore: ["node_modules/**", "src/**"]
+  });
 
-  debug(`Zipping ${files.length} file${files.length === 1 ? '' : 's'}...`)
+  debug(`Zipping ${files.length} file${files.length === 1 ? "" : "s"}...`);
 
-  await tar.create({ gzip: true, follow: true, file: outPath, portable: true }, files)
+  await tar.create(
+    { gzip: true, follow: true, file: outPath, portable: true },
+    files
+  );
 }
